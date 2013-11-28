29f7442f15ced0a49bad9d1fd64a781951f54e91
yigitcan bacakoglu <yigitcan_bacakoglu@hotmail.com>
2013-11-28 15:36:31 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Checkintimer::Application.config.secret_token = '1a07356d4158a78407149859d3fd11eb76c27717c56b1ef1e0e9525e601acc7a37e908934ac8246569d96df5c30461f5ccfe6f9e804ea7d32735178a1ac98ebf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Checkintimer::Application.config.session_store :cookie_store, key: '_checkintimer_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Checkintimer::Application.config.session_store :active_record_store
