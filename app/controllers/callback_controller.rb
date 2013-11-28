11df1aa4cefd64965a32efd46a706467e880507f
yigitcan bacakoglu <yigitcan_bacakoglu@hotmail.com>
2013-11-28 19:23:41 +0200

    foursquare login

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -6,20 +6,24 @@ class CallbackController < ApplicationController
       redirect_to @foursquare.authorize_url(foursquare_callback_url(:host => "127.0.0.1")) and return
     elsif params[:code].present?
       @access_token = @foursquare.access_token(params["code"], (foursquare_callback_url :host => request.host))
-      client = Foursquare2::Client.new(:oauth_token => @access_token)
-      client_user = client.user("self")
-      user = User.find_or_initialize_by_email(client_user["contact"]['email'])
-      temp_pass = SecureRandom.hex(6)
-      user.password ||= temp_pass
-      user.password ||= temp_pass
-      user.firstname = client_user["firstName"]
-      user.lastname = client_user["lastName"]
-      user.gender = client_user["gender"]
-      auth = user.authentications.new
-      auth.profile_picture_url = client_user['photo']
-      auth.uid = client_user['id']
-      auth.oauth_token = @access_token
-      user.save
+      user = User.join(:authentications).where("#{Authentication.table_name}.oauth_token = ?", @access_token).where("#{Authentication.table_name}.provider = ?", "foursquare").first
+      if user.blank?
+        client = Foursquare2::Client.new(:oauth_token => @access_token)
+        client_user = client.user("self")
+        user = User.new(:email => client_user["contact"]['email'])
+        temp_pass = SecureRandom.hex(6)
+        user.password ||= temp_pass
+        user.password ||= temp_pass
+        user.firstname = client_user["firstName"]
+        user.lastname = client_user["lastName"]
+        user.gender = client_user["gender"]
+        auth = user.authentications.new
+        auth.profile_picture_url = client_user['photo']
+        auth.uid = client_user['id']
+        auth.provider = "foursquare"
+        auth.oauth_token = @access_token
+        user.save
+      end
       sign_in(user)
       redirect_to dashboard_path
     else
