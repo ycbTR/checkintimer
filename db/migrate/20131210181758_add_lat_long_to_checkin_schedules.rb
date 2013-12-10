ff17bcbac4c16399a4ce1030b4790c26f0c2ed62
yigitcan bacakoglu <yigitcan_bacakoglu@hotmail.com>
2013-12-10 20:26:21 +0200

    fix

--- db/migrate/20131210181758_add_lat_long_to_checkin_schedules.rb
+++
@@ -1,6 +0,0 @@
-class AddLatLongToCheckinSchedules < ActiveRecord::Migration
-  def change
-    add_column :checkin_schedules, :lat, :string
-    add_column :checkin_schedules, :long, :string
-  end
-end
